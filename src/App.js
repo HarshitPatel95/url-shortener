9d541cc3e9984976b3872e8c4d19e7f776ff923b
Harshit Patel <harshit3228@gmail.com>
2022-09-05 21:58:21 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Link, Routes, Route, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import React, { useState } from "react";
 
 function App() {
+  const location = useLocation();
+  let currentPage = "";
+  if (location.pathname === "/Contact") {
+    currentPage = "contact";
+  } else {
+    currentPage = "home";
+  }
+  const [activeTab, setActiveTab] = useState(currentPage);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar">
+        <span className={`navtabcontainer ${activeTab === "home" ? "active-tab" : ""}`}>
+          <i className="las la-home"></i>
+          <Link
+            className={`${activeTab === "home" ? "active-tab" : ""}`}
+            to="/"
+            onClick={() => setActiveTab("home")}
+          >
+            Home
+          </Link>
+        </span>
+        <span className={`navtabcontainer ${activeTab === "contact" ? "active-tab" : ""}`}>
+          <i className="las la-address-card"></i>
+          <Link
+            className={`${activeTab === "contact" ? "active-tab" : ""}`}
+            to="/Contact"
+            onClick={() => setActiveTab("contact")}
+          >
+            Contact
+          </Link>
+        </span>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="Contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
